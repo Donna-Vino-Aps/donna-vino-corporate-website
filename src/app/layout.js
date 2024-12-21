3c7df8eb8a3b41a0ae58ce3b0b4f3ea85100aa88
lenin del rio <lenindelrionicaragua@gmail.com>
2024-12-21 12:20:44 +0100

    feat: Add data-testid attributes to RootLayout and Cypress tests

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,34 +5,51 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className="flex flex-col min-h-screen w-full">
         {/* Header */}
-        <header>
+        <header data-testid="header">
+          {" "}
+          {/* Added test ID */}
           <nav>
             <ul className="flex bg-red-400 justify-center space-x-6 list-none">
               <li>
-                <a href="/" className="text-lg">
+                <a href="/" className="text-lg" data-testid="home-link">
                   Home
-                </a>
+                </a>{" "}
+                {/* Added test ID */}
               </li>
               <li>
-                <a href="/about" className="text-lg">
+                <a href="/about" className="text-lg" data-testid="about-link">
                   About
-                </a>
+                </a>{" "}
+                {/* Added test ID */}
               </li>
               <li>
-                <a href="/contact" className="text-lg">
+                <a
+                  href="/contact"
+                  className="text-lg"
+                  data-testid="contact-link"
+                >
                   Contact
-                </a>
+                </a>{" "}
+                {/* Added test ID */}
               </li>
             </ul>
           </nav>
         </header>
 
         {/* Main content, will adjust its size based on the content */}
-        <main className=" flex-grow">{children}</main>
+        <main className=" flex-grow" data-testid="main-content">
+          {" "}
+          {/* Added test ID */}
+          {children}
+        </main>
 
         {/* Footer, will always stay at the bottom */}
-        <footer className="bg-gray-800 text-white text-center py-4 mt-auto">
-          © 2024 Donna Vino
+        <footer
+          className="bg-gray-800 text-white text-center py-4 mt-auto"
+          data-testid="footer"
+        >
+          {" "}
+          {/* Added test ID */}© 2024 Donna Vino
         </footer>
       </body>
     </html>
