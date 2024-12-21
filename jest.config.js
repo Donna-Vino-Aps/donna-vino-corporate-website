df30625b7ec6bbf7271064464b0cc9a62bae44b3
lenin del rio <lenindelrionicaragua@gmail.com>
2024-12-21 13:27:30 +0100

    add type: module in package json

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  testEnvironment: "jest-environment-jsdom", // Use the installed jsdom environment
+  testEnvironment: "jsdom", // For React testing environment
   transform: {
-    "^.+\\.jsx?$": "babel-jest", // Transpile JSX files with Babel
+    "^.+\\.[t|j]sx?$": "babel-jest", // Use babel-jest for JS/JSX/TS/TSX files
   },
-  moduleFileExtensions: ["js", "jsx", "json", "node"], // Supported file extensions
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1", // If you use path aliases
+  },
+  transformIgnorePatterns: [
+    "/node_modules/(?!tailwindcss|other-module-to-transform)/", // If you need to transform node_modules
+  ],
 };
--- src/__tests__/app/home.test.js
+++ src/__tests__/app/home.test.js
@@ -1,12 +1,20 @@
 import { render, screen } from "@testing-library/react";
-import Home from "../../app/page";
+import Home from "../../app/page"; // Adjust the import path to where your Home component is located
 
 describe("Home Page", () => {
   it("should render the home page content", () => {
     render(<Home />);
 
-    const heading = screen.getByRole("heading", { level: 1 });
+    // Check that the main heading is in the document
+    const heading = screen.getByTestId("main-heading");
     expect(heading).toBeInTheDocument();
     expect(heading).toHaveTextContent("Welcome to the official Donna Vino");
+
+    // Check that the description is rendered correctly
+    const description = screen.getByTestId("description");
+    expect(description).toBeInTheDocument();
+    expect(description).toHaveTextContent(
+      "This project is dedicated to developing the website for our innovative eCommerce platform, where premium wine sales meet curated tasting experiences."
+    );
   });
 });
