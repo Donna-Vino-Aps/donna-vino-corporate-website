970dc46ee32dddef91108afa02672fb913a9fbe0
lenin del rio <lenindelrionicaragua@gmail.com>
2024-12-21 11:36:33 +0100

    Adjusted styles to center the layout and the home page content

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="flex min-h-[80vh]  items-center justify-center p-8 gap-16 bg-white-500">
+      <main className="flex flex-col gap-8 items-center text-center">
         <h1>Welcome to the official Donna Vino</h1>
         <p>
           This project is dedicated to developing the website for our innovative
