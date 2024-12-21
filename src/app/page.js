0edd16da985d8c70a0f856f133549e8acddff4e2
lenin del rio <lenindelrionicaragua@gmail.com>
2024-12-21 12:20:44 +0100

    feat: Add data-testid attributes to RootLayout and Cypress tests

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,9 @@ export default function Home() {
   return (
     <div className="flex min-h-[80vh]  items-center justify-center p-8 gap-16 bg-white-500">
       <main className="flex flex-col gap-8 items-center text-center">
-        <h1>Welcome to the official Donna Vino</h1>
-        <p>
+        <h1 data-testid="main-heading">Welcome to the official Donna Vino</h1>{" "}
+        {/* Add test ID */}
+        <p data-testid="description">
           This project is dedicated to developing the website for our innovative
           eCommerce platform, where premium wine sales meet curated tasting
           experiences.
