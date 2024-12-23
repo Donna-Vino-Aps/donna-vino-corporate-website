d12e6ee046b851ae1ae42f234f855e8e7afe8d07
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2024-12-23 11:39:51 +0100

    chore:init

---
+++ src/components/Footer/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="flex bg-slate-900">
+      <img src="https://thehub-io.imgix.net/files/s3/20241209183833-4571ce47897c5631412b3cd584fbd674.jpg?fit=crop&w=300&h=300&auto=format&q=60"></img>
+      <div className="grid grid-cols-4">
+        <div>
+            1
+        </div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+      </div>
+    </footer>
+  );
+}
+export default Footer;
