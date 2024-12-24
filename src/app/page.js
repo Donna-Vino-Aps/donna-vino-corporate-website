dc2f1e790a238a3e03f122e53fbe98bed43454e9
lenin del rio <lenindelrionicaragua@gmail.com>
2024-12-24 12:24:54 +0100

    feat: add the new palette color to layout and page layout

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,10 @@ import React from "react";
 export default function Home() {
   return (
     <div className="flex min-h-[80vh] items-center justify-center p-8 gap-16 bg-white">
-      <main className="flex flex-col gap-8 items-center text-center">
+      <main
+        className="flex flex-col gap-8 items-center text-center text-foreground-normal"
+        p
+      >
         <h1 className="text-4xl font-bold" data-testid="main-heading">
           Welcome to the official Donna Vino
         </h1>
