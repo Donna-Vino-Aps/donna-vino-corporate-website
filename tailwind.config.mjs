6b07ae7cb8969a5bb66600ccbf11dd45c03a54b0
lenin del rio <lenindelrionicaragua@gmail.com>
2024-12-24 12:10:56 +0100

    fix: remove colors from darkMode configuration and move to tailwind.config.mjs, removed colors from global.css

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,67 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        primary: {
-          light: "#FDE8E9", // Light Primary
-          hover: "#FCDDDD", // Light Primary Hover
-          active: "#F9B9BB", // Light Primary Active
-          normal: "#ED1C24", // Normal Primary
-          hover_normal: "#D51920", // Normal Primary Hover
-          active_normal: "#BE161D", // Normal Primary Active
-          dark: "#B2151B", // Dark Primary
-          hover_dark: "#8E1116", // Dark Primary Hover
-          active_dark: "#6B0D10", // Dark Primary Active
-          darker: "#530A0D", // Darker Primary
-        },
-        secondary: {
-          light: "#E8ECE9", // Light Secondary
-          hover: "#DCE2DF", // Light Secondary Hover
-          active: "#B7C3BC", // Light Secondary Active
-          normal: "#183127", // Normal Secondary
-          hover_normal: "#163923", // Normal Secondary Hover
-          active_normal: "#133211", // Normal Secondary Active
-          dark: "#12281D", // Dark Secondary
-          hover_dark: "#0E2617", // Dark Secondary Hover
-          active_dark: "#0B1C12", // Dark Secondary Active
-          darker: "#08160E", // Darker Secondary
-        },
-        tertiary1: {
-          light: "#EAEAEA", // Light Tertiary-1
-          hover: "#E0E0E0", // Light Tertiary-1 Hover
-          active: "#BFBEBE", // Light Tertiary-1 Active
-          normal: "#242E2E", // Normal Tertiary-1
-          hover_normal: "#2A2929", // Normal Tertiary-1 Hover
-          active_normal: "#262525", // Normal Tertiary-1 Active
-          dark: "#232323", // Dark Tertiary-1
-          hover_dark: "#1C1C1C", // Dark Tertiary-1 Hover
-          active_dark: "#151515", // Dark Tertiary-1 Active
-          darker: "#101010", // Darker Tertiary-1
-        },
-        tertiary2: {
-          light: "#FEFEFE", // Light Tertiary-2
-          hover: "#FDFDFD", // Light Tertiary-2 Hover
-          active: "#FBFBFB", // Light Tertiary-2 Active
-          normal: "#F1F181", // Normal Tertiary-2
-          hover_normal: "#D9D9D9", // Normal Tertiary-2 Hover
-          active_normal: "#C1C1C1", // Normal Tertiary-2 Active
-          dark: "#B5B5B5", // Dark Tertiary-2
-          hover_dark: "#919191", // Dark Tertiary-2 Hover
-          active_dark: "#6C6C6C", // Dark Tertiary-2 Active
-          darker: "#545454", // Darker Tertiary-2
-        },
-      },
       fontFamily: {
-        barlow: ["Barlow", "sans-serif"], // main font
-        roboto: ["Roboto", "sans-serif"], // secondary font
+        barlow: ["Barlow", "sans-serif"], // Main font
+        roboto: ["Roboto", "sans-serif"], // Secondary font
       },
       fontSize: {
         displayLarge: [
@@ -89,7 +36,58 @@ export default {
         medium: "500",
         semibold: "600",
       },
+      colors: {
+        primary: {
+          light: "#FDE8E9",
+          hover: "#FCDDDD",
+          active: "#F9B9BB",
+          normal: "#ED1C24",
+          hover_normal: "#D51920",
+          active_normal: "#BE161D",
+          dark: "#B2151B",
+          hover_dark: "#8E1116",
+          active_dark: "#6B0D10",
+          darker: "#530A0D",
+        },
+        secondary: {
+          light: "#E8ECE9",
+          hover: "#DCE2DF",
+          active: "#B7C3BC",
+          normal: "#183127",
+          hover_normal: "#163923",
+          active_normal: "#133211",
+          dark: "#12281D",
+          hover_dark: "#0E2617",
+          active_dark: "#0B1C12",
+          darker: "#08160E",
+        },
+        tertiary1: {
+          light: "#EAEAEA",
+          hover: "#E0E0E0",
+          active: "#BFBEBE",
+          normal: "#242E2E",
+          hover_normal: "#2A2929",
+          active_normal: "#262525",
+          dark: "#232323",
+          hover_dark: "#1C1C1C",
+          active_dark: "#151515",
+          darker: "#101010",
+        },
+        tertiary2: {
+          light: "#FEFEFE",
+          hover: "#FDFDFD",
+          active: "#FBFBFB",
+          normal: "#F1F181",
+          hover_normal: "#D9D9D9",
+          active_normal: "#C1C1C1",
+          dark: "#B5B5B5",
+          hover_dark: "#919191",
+          active_dark: "#6C6C6C",
+          darker: "#545454",
+        },
+      },
     },
   },
+  darkMode: "class", // Enable dark mode using a class
   plugins: [],
 };
