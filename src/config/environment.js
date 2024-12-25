352baced98f06f62a2fdd8074ddf021540fd6a0f
lenin del rio <lenindelrionicaragua@gmail.com>
2024-12-25 17:51:18 +0100

    Refactor: Renamed test folders to avoid confusion with real folders
    Renamed test folder names to prevent conflicts with the production directory names, improving project organization and clarity.

--- src/config/environment.js
+++ src/config/environment.js
@@ -5,6 +5,9 @@ import { API_URL_PRODUCTION, API_URL_DEVELOPMENT } from "@env";
 const DEFAULT_API_URL_PRODUCTION = "https://exampleurlproduction.com";
 const DEFAULT_API_URL_DEVELOPMENT = "http://exampleurldevelopment:3000";
 
+// Note: Currently, we do not have the key for Heroku, so we cannot update the secret variables.
+// Once available, we will update this file with the real URLs.
+
 // Determine the base API URL based on the environment
 const isProduction = process.env.NODE_ENV === "production";
 export const baseApiUrl = isProduction
