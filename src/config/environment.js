49d0f3e4cedc6a23550e9d7f7e266da4bd4208c4
lenin del rio <lenindelrionicaragua@gmail.com>
2024-12-25 18:46:48 +0100

    feat: Use Next.js environment variables and add temporary fallback URLs in .env file, switching variables based on environment

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,17 +1,7 @@
-import { API_URL_PRODUCTION, API_URL_DEVELOPMENT } from "@env";
-// import { logInfo } from "../../util/loading";
-
-// Define temporarily default URLs for fallback; we will update this soon
-const DEFAULT_API_URL_PRODUCTION = "https://exampleurlproduction.com";
-const DEFAULT_API_URL_DEVELOPMENT = "http://exampleurldevelopment:3000";
-
-// Note: Currently, we do not have the key for Heroku, so we cannot update the secret variables.
-// Once available, we will update this file with the real URLs.
-
 // Determine the base API URL based on the environment
 const isProduction = process.env.NODE_ENV === "production";
-export const baseApiUrl = isProduction
-  ? API_URL_PRODUCTION || DEFAULT_API_URL_PRODUCTION
-  : API_URL_DEVELOPMENT || DEFAULT_API_URL_DEVELOPMENT;
 
-// logInfo(`Server URL: ${baseApiUrl}`);
+// If it is production, we take the production URL, otherwise we take the development URL
+export const baseApiUrl = isProduction
+  ? process.env.NEXT_PUBLIC_API_URL_PRODUCTION || "https://defaulturl.com"
+  : process.env.NEXT_PUBLIC_API_URL_DEVELOPMENT || "http://localhost:3000";
