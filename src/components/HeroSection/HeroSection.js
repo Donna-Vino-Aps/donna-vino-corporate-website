f437b824cdefe38f2125b88992ad77498c4c0778
lenin del rio <lenindelrionicaragua@gmail.com>
2024-12-28 15:15:39 +0100

    fix: update the size of the Hero component

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const HeroSection = () => {
   return (
-    <section className="relative flex flex-row w-800 h-450 bg-primary-light">
-      {/* Overlay Text*/}
-      <div className="relative flex flex-col justify-center items-start text-left w-full p-8">
-        <h1 className="text-displayLarge  text-tertiary1-normal font-light font-barlow mb-4">
+    <section className="relative flex flex-row justify-between  px-8 gap-x-8 w-[1440px] h-[704px] bg-primary-light">
+      {/* Overlay Text */}
+      <div className="relative flex flex-col justify-center items-start text-left w-[704px] h-[704px] p-8">
+        <h1 className="text-displayLarge text-tertiary1-normal font-light font-barlow mb-4">
           Welcome to Donna Vino, your unique wine experience.
         </h1>
         <p
@@ -18,15 +18,15 @@ const HeroSection = () => {
       </div>
 
       {/* Video Background */}
-      <div className="relative flex flex-col justify-center items-start text-left w-full">
+      <div className="relative flex flex-col justify-center items-start text-left w-[720px] h-[704px]">
         <video
-          className="absolute inset-0 w-[60vh] h-[40vh] object-cover autoPlay loop muted"
+          className="absolute inset-0 w-full h-full object-cover"
           autoPlay
           loop
           muted
           playsInline
         >
-          <source src="/videos/Sequence 01.mp4" type="video/mp4" />
+          <source src="/videos/Sequence01.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
