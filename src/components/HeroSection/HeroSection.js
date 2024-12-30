c26ec8cbe28290c7c137a560dfd655d73442d9cb
lenin del rio <lenindelrionicaragua@gmail.com>
2024-12-30 17:38:19 +0100

    feat: add container for buttons and adjust button proportions

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -3,13 +3,13 @@ import React from "react";
 const HeroSection = () => {
   return (
     <section
-      className="relative flex flex-col w-full sm:flex-row h-auto bg-primary-light"
+      className="relative flex flex-col w-screen sm:flex-row h-auto bg-primary-light overflow-hidden"
       role="banner"
       aria-label="Hero Section"
       aria-labelledby="title"
     >
       <div
-        className="relative flex flex-col gap-8 justify-center h-[60%] text-left p-4 pt-8"
+        className="relative flex flex-col gap-8 justify-center h-[60%] text-left p-4 pt-8 m-0"
         data-testid="hero-text-container"
       >
         <h1
@@ -26,22 +26,28 @@ const HeroSection = () => {
           Discover unique wine stories told by your sommelier while your private
           chef customizes the menu.
         </p>
-        <button
-          className="bg-primary-normal text-white w-[332px] h-[48px] rounded-[6px] px-[24px] py-[12px] flex justify-center items-center"
-          data-testid="get-started-button"
-        >
-          <img src="/icons/cart.svg" alt="Cart" className="mr-2" />
-          Visit our shop
-        </button>
-        <button
-          className="bg-primary-light border-2 border-primary-normal text-primary-normal w-[332px] h-[48px] rounded-[6px] px-[24px] py-[12px] flex justify-center items-center
+        <div className="flex flex-col gap-4 w-full">
+          <button
+            className="bg-primary-normal text-white w-full h-[3.25rem] rounded-[6px] px-[24px] py-[12px] flex justify-center items-center"
+            data-testid="get-started-button"
+          >
+            <img src="/icons/cart.svg" alt="Cart" className="mr-2" />
+            Visit our shop
+          </button>
+          <button
+            className="bg-primary-light border-2 border-primary-normal text-primary-normal w-full h-[3rem] rounded-[6px] px-[24px] py-[12px] flex justify-center items-center
           hover:border-primary-normal-active hover:text-primary-normal-active 
           active:border-primary-normal-active active:text-primary-normal-active"
-          data-testid="secondary-button"
-        >
-          <img src="/icons/speech-bubble-19.svg" alt="Cart" className="mr-2" />
-          Contact us
-        </button>
+            data-testid="secondary-button"
+          >
+            <img
+              src="/icons/speech-bubble-19.svg"
+              alt="Cart"
+              className="mr-2"
+            />
+            Contact us
+          </button>
+        </div>
       </div>
 
       <div className="relative justify-center w-full h-auto sm:w-40% sm:h-auto">
