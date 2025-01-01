39b09410c9961cc3473862fa3059854b36ed2efe
lenin del rio <lenindelrionicaragua@gmail.com>
2025-01-01 15:54:51 +0100

    fix: Refactor button styles to use responsive units (rem) for better scalability and flexibility.

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -3,13 +3,13 @@ import React from "react";
 const HeroSection = () => {
   return (
     <section
-      className="relative flex flex-col w-screen sm:flex-row h-auto bg-primary-light"
+      className="relative flex flex-col w-screen sm:flex-row min-h-[44rem] bg-primary-light"
       role="banner"
       aria-label="Hero Section"
       aria-labelledby="title"
     >
       <div
-        className="relative flex flex-col gap-8 justify-center h-[60%] text-left p-4 pt-8 m-0"
+        className="relative flex flex-col gap-8 justify-center w-[44rem] h-[44rem] text-left p-4 pt-8 m-0"
         data-testid="hero-text-container"
       >
         <div className="flex justify-start">
@@ -28,7 +28,7 @@ const HeroSection = () => {
         </div>
 
         <h1
-          className="text-displayLarge text-tertiary1-normal font-light text-[45px] leading-[52px] tracking-[0] font-barlow pt-1"
+          className="text-displayLarge text-tertiary1-normal font-light font-barlow pt-1"
           id="title"
           data-testid="title"
         >
@@ -42,26 +42,26 @@ const HeroSection = () => {
           chef customizes the menu.
         </p>
         <div className="flex flex-col gap-4 w-full">
-          <button
-            className="bg-primary-normal text-white w-full h-[3.25rem] rounded-[6px] px-[24px] py-[12px] flex justify-center items-center"
-            data-testid="get-started-button"
-          >
-            <img src="/icons/cart.svg" alt="Cart" className="mr-2" />
-            Visit our shop
-          </button>
-          <button
-            className="bg-primary-light border-2 border-primary-normal text-primary-normal w-full h-[3rem] rounded-[6px] px-[24px] py-[12px] flex justify-center items-center
-          hover:border-primary-normal-active hover:text-primary-normal-active 
-          active:border-primary-normal-active active:text-primary-normal-active"
-            data-testid="secondary-button"
-          >
-            <img
-              src="/icons/speech-bubble-19.svg"
-              alt="Cart"
-              className="mr-2"
-            />
-            Contact us
-          </button>
+          <div className="flex flex-row gap-4 w-full">
+            <button
+              className="bg-primary-normal text-white w-[10.8rem] h-[3rem] rounded-[0.3rem] flex justify-center items-center"
+              data-testid="get-started-button"
+            >
+              <img src="/icons/cart.svg" alt="Cart" className="mr-2" />
+              Visit our shop
+            </button>
+            <button
+              className="bg-primary-light border-2 border-primary-normal text-primary-normal w-[10.8rem] h-[3rem] rounded-[0.3rem] flex justify-center items-center"
+              data-testid="secondary-button"
+            >
+              <img
+                src="/icons/speech-bubble-19.svg"
+                alt="Cart"
+                className="mr-2"
+              />
+              Contact us
+            </button>
+          </div>
           <div className="flex justify-end">
             <div className="relative w-[13.75rem] h-[8.68rem]">
               <img
@@ -79,9 +79,9 @@ const HeroSection = () => {
         </div>
       </div>
 
-      <div className="relative justify-center w-full h-auto sm:w-40% sm:h-auto">
+      <div className="relative justify-center  w-[44rem] h-[44rem] sm:w-[45rem] sm:h-[44rem]">
         <video
-          className="inset-0 w-full sm:h-full object-cover"
+          className="inset-0 w-[45rem] sm:h-[44rem] object-cover"
           autoPlay
           loop
           muted
