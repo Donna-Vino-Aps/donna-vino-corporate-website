7181246b8c1f532a8c299b9c1e186a0f9c5c9456
lenin del rio <lenindelrionicaragua@gmail.com>
2025-01-04 19:39:36 +0100

    Add basic Jest tests for Button component
    - Created tests to check rendering of button text and icon.
    - Added test for onClick function execution.
    - Included tests for dynamic class application based on variant and button disabling state.

---
+++ src/__tests__/componentsTest/Button.test.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import Button from "../../components/Button/Button";
+
+describe("Button Component", () => {
+  it("should render button with text", () => {
+    render(<Button text="Visit our shop" />);
+    const button = screen.getByText(/Visit our shop/i);
+    expect(button).toBeInTheDocument();
+  });
+
+  it("should render button with an icon", () => {
+    render(<Button text="Visit our shop" icon="/icons/cart.svg" />);
+    const icon = screen.getByAltText(/Icon/i);
+    expect(icon).toBeInTheDocument();
+  });
+
+  it("should call onClick function when clicked", () => {
+    const handleClick = jest.fn();
+    render(<Button text="Visit our shop" onClick={handleClick} />);
+    const button = screen.getByText(/Visit our shop/i);
+    fireEvent.click(button);
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+
+  it("should apply the correct class based on variant", () => {
+    const { container } = render(
+      <Button text="Visit our shop" variant="primary" />,
+    );
+    const button = container.querySelector("button");
+    expect(button).toHaveClass("bg-primary-normal");
+  });
+
+  it("should disable the button when disabled is true", () => {
+    render(<Button text="Visit our shop" disabled />);
+    const button = screen.getByText(/Visit our shop/i);
+    expect(button).toBeDisabled();
+  });
+});
