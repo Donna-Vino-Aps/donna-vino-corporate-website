4ad57bac976dba33039e435ddf88f071fe12d125
Ana Gabriela <anagabrielaguz26@gmail.com>
2025-01-04 16:14:13 +0100

    - Implemented Link in Navbar Component. - Implemented LanguageSwitch. - Implemented Unit Test.

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,48 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./globals.css";
 import Footer from "../components/Footer/Footer.js";
+import Navbar from "../components/Navbar/Navbar.js";
 // import { logInfo } from "@/utils/logging";
 
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-      <body className="flex flex-col min-h-screen w-full font-barlow bg-primary-normal text-foreground-normal">
-        <header data-testid="header" role="banner">
-          <nav role="navigation">
-            <ul className="flex bg-primary-normal justify-center space-x-6 list-none">
-              <li>
-                <a
-                  href="/"
-                  className="text-lg font-roboto"
-                  data-testid="home-link"
-                  aria-label="Go to home page"
-                >
-                  Home
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/about"
-                  className="text-lg font-roboto"
-                  data-testid="about-link"
-                  aria-label="Go to about page"
-                >
-                  About
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/contact"
-                  className="text-lg font-roboto"
-                  data-testid="contact-link"
-                  aria-label="Go to contact page"
-                >
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </header>
+      <body className="flex flex-col min-h-screen w-full pt-20 font-barlow bg-primary-normal text-foreground-normal">
+        <Navbar />
 
         <main className="flex-grow" role="main" data-testid="main-content">
           {children}
---
+++ src/components/Navbar/LanguageSwitch.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import useStickyState from '@/hooks/helpers/useStickyState';
+
+function LanguageSwitch() {
+  const [selectedLanguage, setLanguage] = useStickyState('en', 'pageLanguage'); 
+
+  return (
+    <div className="flag-switch flex">
+      <div 
+        data-testid= "en-icon"
+        className={`w-1/2 p-2 rounded-md hover:bg-primary-light
+        ${selectedLanguage === 'en' ? 'bg-primary-light' : ''}`}
+        onClick={() => setLanguage('en')} 
+      >
+        <img src="/images/ic_en.png" alt="en" />
+      </div>
+      <div 
+      data-testid= "dk-icon"
+        className={`w-1/2 p-2 rounded-md hover:bg-primary-light
+        ${selectedLanguage === 'dk' ? 'bg-primary-light' : ''}`}
+        onClick={() => setLanguage('dk')} 
+      >
+        <img src="/images/ic_dk.png" alt="dk" />
+      </div>
+    </div>
+  );
+}
+
+export default LanguageSwitch;
