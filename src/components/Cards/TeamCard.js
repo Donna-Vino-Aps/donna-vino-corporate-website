9bd47d11267b367ec4618c710f852c80a2c3caca
Ana Gabriela <anagabrielaguz26@gmail.com>
2025-01-06 15:17:52 +0100

    test card

--- src/components/Cards/TeamCard.js
+++ src/components/Cards/TeamCard.js
@@ -2,9 +2,14 @@ import React from "react";
 
 const TeamCard = () => {
   return (
-    <div className="relative rounded overflow-hidden shadow-lg m-2">
+    <div
+      className="relative rounded overflow-hidden shadow-lg m-2"
+      aria-label="Team Card"
+      data-testid="team-card"
+    >
       <img className="w-full" src="/images/foto-katrine.png" alt="" />
       <button
+        role="button"
         className="absolute bottom-0 left-0 right-0 mb-4 mx-4 bg-primary-normal text-white 
              rounded px-6 py-2 hover:bg-primary-dark"
       >
