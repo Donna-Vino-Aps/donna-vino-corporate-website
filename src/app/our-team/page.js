e0eae5ccbe74d5b32a13286414d99db4e8181c9f
Ana Gabriela <anagabrielaguz26@gmail.com>
2025-01-06 14:27:37 +0100

    implemented grid and the card team design

--- src/app/our-team/page.js
+++ src/app/our-team/page.js
@@ -1,7 +1,31 @@
 import React from "react";
+import TeamCard from "@/components/Cards/TeamCard";
 
 const OurTeam = () => {
-  return <h1 className="text-white mt-10">Our Team page</h1>;
+  return (
+    <div className="flex justify-center">
+      <div className="mt-10">
+        <p className="text-center text-primary-normal mt-6">#OneTeamOneDream</p>
+        <h2 className="text-displayMedium md:text-displayLarge text-center">
+          Our Awesome Team
+        </h2>
+        <div className="flex flex-col justify-center items-center w-full">
+          <p className="text-titleMedium text-tertiary1-gray mt-4 text-center mb-4 max-w-[476px]">
+            There are many variations of passages of Lorem Ipsum available but
+            the majority have suffered alteration in some form.
+          </p>
+        </div>
+        <div className="flex flex-wrap mx-6 gap-4 justify-center">
+          <TeamCard />
+          <TeamCard />
+          <TeamCard />
+          <TeamCard />
+          <TeamCard />
+          <TeamCard />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default OurTeam;
---
+++ src/components/Cards/TeamCard.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const TeamCard = () => {
+  return (
+    <div className="relative rounded overflow-hidden shadow-lg m-2">
+      <img className="w-full" src="/images/foto-katrine.png" alt="" />
+      <button
+        className="absolute bottom-0 left-0 right-0 mb-4 mx-4 bg-primary-normal text-white 
+             rounded px-6 py-2 hover:bg-primary-dark"
+      >
+        <p class="text-bodyLarge">Katrine Giogio</p>
+        <p className="m-2 text-labelMedium">Founder</p>
+        <div className="flex justify-center text-white">
+          <img
+            src="/icons/linkedin-alt-light.svg"
+            className="h-4"
+            alt="LinkedIn"
+          />
+        </div>
+      </button>
+    </div>
+  );
+};
+
+export default TeamCard;
