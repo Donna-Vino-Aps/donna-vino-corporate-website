4856a05de9f486636a8a248f29dad8a98ce6b841
Ana Gabriela <anagabrielaguz26@gmail.com>
2025-01-07 11:48:30 +0100

    added activeLink in navbar

--- src/components/Cards/TeamCard.js
+++ src/components/Cards/TeamCard.js
@@ -13,7 +13,7 @@ const TeamCard = () => {
         className="absolute bottom-0 left-0 right-0 mb-4 mx-4 bg-primary-normal text-white 
              rounded px-6 py-2 hover:bg-primary-dark"
       >
-        <p class="text-bodyLarge">Katrine Giogio</p>
+        <p className="text-bodyLarge">Katrine Giogio</p>
         <p className="m-2 text-labelMedium">Founder</p>
         <div className="flex justify-center text-white">
           <img
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,6 +6,8 @@ import LanguageSwitch from "./LanguageSwitch";
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  const [activeLink, setActiveLink] = useState("/");
+
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
@@ -16,6 +18,10 @@ const Navbar = () => {
     { id: "our-team", href: "/our-team", label: "Our Team" },
     { id: "contact", href: "/contact", label: "Contact" },
   ];
+
+  const handleClick = (href) => {
+    setActiveLink(href);
+  };
 
   return (
     <nav className="fixed top-0 left-0 w-full z-50 bg-white shadow-md">
@@ -28,12 +34,7 @@ const Navbar = () => {
           />
         </Link>
         <div className="md:hidden mx-4">
-          <button
-            className="block"
-            onClick={toggleMenu}
-            role="button"
-            aria-label="menu"
-          >
+          <button onClick={toggleMenu} role="button" aria-label="menu">
             <img src="/icons/menu.svg" alt="" className="mr-2" />
           </button>
         </div>
@@ -47,7 +48,12 @@ const Navbar = () => {
               data-testid={link.id}
               key={link.label}
               href={link.href}
-              className="rounded-md px-3 py-2 text-titleMedium text-tertiary1"
+              className={`rounded-md px-3 py-2 text-titleMedium ${
+                activeLink === link.href
+                  ? "text-tertiary1-darker"
+                  : "text-tertiary1-gray"
+              }`}
+              onClick={() => handleClick(link.href)}
               role="navigation"
             >
               {link.label}
