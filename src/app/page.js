c7c259f3e6160d28105e6de9f52944c5d04a39f6
lenin del rio <lenindelrionicaragua@gmail.com>
2025-01-09 15:43:36 +0100

    fix:merge conflicts

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,8 @@ import React from "react";
 import HeroSection from "@/components/HeroSection/HeroSection";
 import Subscribe from "@/components/Subscribe/Subscribe";
 import PhotoCardsWineTasting from "@/components/ContentGrid/PhotoCardsWineTasting";
-<<<<<<< HEAD
 import PhotoCardsTeamValues from "@/components/ContentGrid/PhotoCardsTeamValues";
 // import { logInfo } from "@/utils/logging";
-=======
->>>>>>> 3cabf9b (chore: add test IDs for better testability in components and page)
 
 export default function Home() {
   return (
@@ -16,12 +13,10 @@ export default function Home() {
       data-testid="home-container"
     >
       <main data-testid="main-heading">
-
         <HeroSection />
         <PhotoCardsWineTasting />
         <Subscribe />
         <PhotoCardsTeamValues />
-
       </main>
     </div>
   );
