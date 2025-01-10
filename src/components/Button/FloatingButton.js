dcac5555e74b1459cfeac8d700179e6f96d50233
Ana Gabriela <anagabrielaguz26@gmail.com>
2025-01-10 12:47:34 +0100

    added floatingButton test

--- src/components/Button/FloatingButton.js
+++ src/components/Button/FloatingButton.js
@@ -5,10 +5,12 @@ const FloatingButton = () => {
     <button
       className="bg-primary-active_normal w-8 h-8
             rounded-full shadow-lg flex justify-center items-center"
+      data-testid="floating-button"
     >
       <img
         src="/icons/linkedin-alt-light.svg"
         alt="social media"
+        data-testid="social-icon"
         className="w-4"
       />
     </button>
