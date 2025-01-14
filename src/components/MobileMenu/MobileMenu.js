9a3914d2910a9c34b4d6ec2d1ab6967544151557
lenin del rio <lenindelrionicaragua@gmail.com>
2025-01-14 12:25:53 +0100

    refactor: move mobile menu logic to separate component

---
+++ src/components/MobileMenu/MobileMenu.js
@@ -0,0 +1,83 @@
+import Link from "next/link";
+import LanguageSwitch from "./LanguageSwitch";
+
+const MobileMenu = ({ isMenuOpen, toggleMenu, navLinks }) => {
+  return (
+    <div
+      className={`fixed right-0 top-0 w-full h-full sm:hidden z-40 ${
+        isMenuOpen ? "translate-x-0" : "translate-x-full"
+      }`}
+    >
+      <div className="flex-col h-full pt-6 px-6 bg-white">
+        <div className="flex justify-between pb-6">
+          <div className="flex items-center">
+            <img
+              src="/images/donna-vino-logo-transparent.png"
+              alt="Donna Vino's logo"
+              className="h-16 rounded logo"
+            />
+          </div>
+          <button
+            className="self-end mb-6 mx-4"
+            onClick={toggleMenu}
+            role="button"
+            aria-label="close"
+          >
+            <div>
+              <img src="/icons/close.svg" alt="" className="mr-2 w-5" />
+            </div>
+          </button>
+        </div>
+        <ul>
+          {navLinks.map((link) => (
+            <li key={link.label}>
+              <Link
+                href={link.href}
+                onClick={toggleMenu}
+                className="block py-2 text-bodyLarge text-tertiary1"
+                role="navigation"
+              >
+                {link.label}
+              </Link>
+            </li>
+          ))}
+          <hr className="my-2 border-t-slate-300" />
+          <div className="mt-12 flex col-6">
+            <LanguageSwitch />
+          </div>
+          <div className="pt-12">
+            <h4 className="pb-3 font-semibold footerHeading">Follow Us On</h4>
+            <div
+              className="flex gap-4 m-1 relative right-1"
+              aria-label="Social media icons"
+            >
+              <a href="#" className="text-black">
+                <img
+                  src="/icons/instagram-original.svg"
+                  className="h-4 filter brightness-0"
+                  alt="Instagram"
+                />
+              </a>
+              <a href="#" className="text-black">
+                <img
+                  src="/icons/linkedin-alt.svg"
+                  className="h-4 filter brightness-0"
+                  alt="LinkedIn"
+                />
+              </a>
+              <a href="#" className="text-black">
+                <img
+                  src="/icons/facebook-line.svg"
+                  className="h-4 filter brightness-0"
+                  alt="Facebook"
+                />
+              </a>
+            </div>
+          </div>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default MobileMenu;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import React, { useState } from "react";
 import LanguageSwitch from "./LanguageSwitch";
 import { useLanguage } from "@/app/context/LanguageContext";
+import MobileMenu from "../MobileMenu/MobileMenu";
 
 const Navbar = () => {
   const { translations } = useLanguage();
@@ -29,22 +30,25 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="w-full z-50 bg-black shadow-md">
-      <div className="container py-8 px-6 flex justify-between items-center">
-        <Link href="/" className="navbar-brand" aria-label="logo">
+    <nav className="w-full h-[7.18rem] z-50 shadow-md">
+      <div
+        className="flex flex-col-1 justify-between items-center"
+        data-testid="container"
+      >
+        <Link href="/" data-testid="navbar-brand" aria-label="logo">
           <img
             src="/images/donna-vino-logo-transparent.png"
             alt="logo"
-            className="w-[86px] rounded logo"
+            className="w-[6.25rem] h-[4.31rem]"
           />
         </Link>
-        <div className="md:hidden mx-4">
+        <div className="sm:hidden mx-4">
           <button onClick={toggleMenu} role="button" aria-label="menu">
             <img src="/icons/menu.svg" alt="" className="mr-2" />
           </button>
         </div>
         <div
-          className={`md:flex md:items-center md:space-x-4 ${
+          className={`sm:flex sm:items-center sm:space-x-4 ${
             isMenuOpen ? "block" : "hidden"
           }`}
         >
@@ -65,85 +69,16 @@ const Navbar = () => {
             </Link>
           ))}
         </div>
-        <div className="hidden  sm:block">
+        <div className="hidden sm:block">
           <LanguageSwitch />
         </div>
       </div>
 
-      <div
-        className={`fixed right-0 top-0 w-full  h-full sm:hidden z-40 ${
-          isMenuOpen ? "translate-x-0" : "translate-x-full"
-        }`}
-      >
-        <div className="flex-col h-full pt-6 px-6 bg-white">
-          <div className="flex justify-between pb-6">
-            <div className="flex items-center">
-              <img
-                src="/images/donna-vino-logo-transparent.png"
-                alt="Donna Vino's logo"
-                className="h-16 rounded logo"
-              />
-            </div>
-            <button
-              className="self-end mb-6 mx-4"
-              onClick={toggleMenu}
-              role="button"
-              aria-label="close"
-            >
-              <div>
-                <img src="/icons/close.svg" alt="" className="mr-2 w-5" />
-              </div>
-            </button>
-          </div>
-          <ul>
-            {navLinks.map((link) => (
-              <li key={link.label}>
-                <Link
-                  href={link.href}
-                  onClick={toggleMenu}
-                  className="block py-2 text-bodyLarge text-tertiary1"
-                  role="navigation"
-                >
-                  {link.label}
-                </Link>
-              </li>
-            ))}
-            <hr className="my-2 border-t-slate-300" />
-            <div className="mt-12 flex col-6">
-              <LanguageSwitch />
-            </div>
-            <div className="pt-12">
-              <h4 className="pb-3 font-semibold footerHeading">Follow Us On</h4>
-              <div
-                className="flex gap-4 m-1 relative right-1"
-                aria-label="Social media icons"
-              >
-                <a href="#" className="text-black">
-                  <img
-                    src="/icons/instagram-original.svg"
-                    className="h-4 filter brightness-0"
-                    alt="Instagram"
-                  />
-                </a>
-                <a href="#" className="text-black">
-                  <img
-                    src="/icons/linkedin-alt.svg"
-                    className="h-4 filter brightness-0"
-                    alt="LinkedIn"
-                  />
-                </a>
-                <a href="#" className="text-black">
-                  <img
-                    src="/icons/facebook-line.svg"
-                    className="h-4 filter brightness-0"
-                    alt="Facebook"
-                  />
-                </a>
-              </div>
-            </div>
-          </ul>
-        </div>
-      </div>
+      <MobileMenu
+        isMenuOpen={isMenuOpen}
+        toggleMenu={toggleMenu}
+        navLinks={navLinks}
+      />
     </nav>
   );
 };
