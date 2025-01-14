efe4be912cd44b3630a56ec1d21bea4729cec8d6
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-01-14 16:37:33 +0100

    fix: refactor the 6 final tests after translation

--- src/__tests__/componentsTest/ThematicCardsValues.test.js
+++ src/__tests__/componentsTest/ThematicCardsValues.test.js
@@ -1,11 +1,38 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import ThematicCardsValues from "@/components/Card/ThematicCard";
+import { LanguageProvider } from "@/app/context/LanguageContext";
+import enTranslations from "../../translations/en.json";
 
 describe("ThematicCardsValues Component", () => {
+  const defaultProps = {
+    imageUrl: "/test-image.jpg",
+    imgPos: "left",
+    smallCardSize: "medium",
+    title: "Test Title",
+    descriptionStart: "This is the start of the description.",
+    descriptionEnd: "This is the end of the description.",
+    backgroundColor: "#ffffff",
+  };
+
+  // Validate that defaultProps match the expected prop types
+  it("should pass prop validation with default props", () => {
+    PropTypes.checkPropTypes(
+      ThematicCardsValues.propTypes,
+      defaultProps,
+      "prop",
+      ThematicCardsValues.name,
+    );
+  });
+
   it("renders without crashing", () => {
-    render(<ThematicCardsValues />);
+    render(
+      <LanguageProvider value={enTranslations}>
+        <ThematicCardsValues />
+      </LanguageProvider>,
+    );
   });
 
   it("displays the 'Show more' button in small screen layout", () => {
@@ -13,7 +40,11 @@ describe("ThematicCardsValues Component", () => {
     global.innerWidth = 800;
     global.dispatchEvent(new Event("resize"));
 
-    render(<ThematicCardsValues />);
+    render(
+      <LanguageProvider value={enTranslations}>
+        <ThematicCardsValues />
+      </LanguageProvider>,
+    );
 
     // Verify 'Show more' button is visible on small screens
     const showMoreButton = screen.queryByText("Show more");
@@ -25,7 +56,11 @@ describe("ThematicCardsValues Component", () => {
     global.innerWidth = 1200;
     global.dispatchEvent(new Event("resize"));
 
-    render(<ThematicCardsValues />);
+    render(
+      <LanguageProvider value={enTranslations}>
+        <ThematicCardsValues />
+      </LanguageProvider>,
+    );
 
     // Verify 'Show more' button is not visible on large screens
     const showMoreButton = screen.queryByText("Show more");
