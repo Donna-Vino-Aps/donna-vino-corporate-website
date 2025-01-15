f50186967a8298ce696cbd8d217fc30784cdfaa1
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-01-15 13:26:48 +0100

    chore: init

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,22 +6,14 @@ import { useLanguage } from "@/app/context/LanguageContext";
 
 const Footer = () => {
   const { translations } = useLanguage();
-  const navLinks = [
-    {
-      id: "our-values",
-      href: "/our-values",
-      label: translations["footer.values"],
-    },
-    { id: "our-team", href: "/our-team", label: translations["footer.team"] },
-    { id: "contact", href: "/contact", label: translations["footer.contact"] },
-  ];
+
   return (
     <footer
       className="flex text-white text-center py-4 h-96 bg-[#2F2E2E] md:h-[26.625rem] items-start justify-center"
       data-testid="footer"
       aria-label="Footer"
     >
-      <div className="grid grid-cols-1 relative top-2 gap-1 md:top-0 md:grid-cols-5 md:mt-24 md:gap-5 lg:gap-8 xl:gap-11 ">
+      <div className="grid grid-cols-1 relative top-2 gap-1 md:top-0 md:grid-cols-5 md:mt-24 md:gap-5 lg:gap-8 xl:gap-11">
         <Link href="/" className="navbar-brand" aria-label="logo">
           <img
             className="h-20 rounded relative mt-4 mb-4 md:mt-0 md:mb-0 md:right-4"
@@ -30,19 +22,37 @@ const Footer = () => {
             data-testid="logo-footer"
           ></img>
         </Link>
-        {navLinks.map((link) => (
-          <Link
-            data-testid={link.id}
-            key={link.label}
-            href={link.href}
-            className="rounded-md px-3 py-2 text-bodyLarge text-semibold"
-            role="navigation"
-            aria-label={`Link to ${link.label}`}
-          >
-            {link.label}
-          </Link>
-        ))}
-        <div className="p-2">
+        <Link
+          data-testid={"our-team"}
+          label={translations["footer.team"]}
+          href="/our-team"
+          className="rounded-md px-3 py-2 text-bodyLarge text-semibold order-4 md:order-1"
+          role="navigation"
+          aria-label={`Link to ${translations["footer.team"]}`}
+        >
+          {translations["footer.team"]}
+        </Link>
+        <Link
+          data-testid={"our-values"}
+          label={translations["footer.values"]}
+          href="/our-values"
+          className="rounded-md px-3 py-2 text-bodyLarge text-semibold order-3 md:order-2"
+          role="navigation"
+          aria-label={`Link to ${translations["footer.values"]}`}
+        >
+          {translations["footer.values"]}
+        </Link>
+        <Link
+          data-testid={"contact"}
+          label={translations["footer.contact"]}
+          href="/contact"
+          className="rounded-md px-3 py-2 text-bodyLarge text-semibold order-2 md:order-3"
+          role="navigation"
+          aria-label={`Link to ${translations["footer.contact"]}`}
+        >
+          {translations["footer.contact"]}
+        </Link>
+        <div className="p-2 order-1 md:order-4">
           <h4 className="pb-3 text-bodyLarge text-semibold">
             {translations["footer.follow"]}
           </h4>
