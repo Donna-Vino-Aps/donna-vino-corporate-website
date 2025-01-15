641bdbb86b72aef4da538f9b617a0581e14cfdeb
lenin del rio <lenindelrionicaragua@gmail.com>
2025-01-15 12:42:52 +0100

    backup: commiting before pull main

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -14,8 +14,9 @@ const MobileMenu = ({ isMenuOpen, toggleMenu, navLinks }) => {
       aria-hidden={!isMenuOpen}
       aria-labelledby="mobile-menu-heading"
     >
-      <div className="flex-col h-full pt-6 px-6 bg-white">
-        <div className="flex justify-between pb-6">
+      <div className="flex flex-col h-full pt-6 px-6 bg-white">
+        {/* Logo and Close Button Container */}
+        <div className="flex justify-between mb-6">
           <div className="flex items-center">
             <img
               src="/images/donna-vino-logo-transparent.png"
@@ -24,62 +25,68 @@ const MobileMenu = ({ isMenuOpen, toggleMenu, navLinks }) => {
             />
           </div>
           <button
-            className="self-end mb-6 mx-4"
+            className="self-start"
             onClick={toggleMenu}
             aria-label="Close menu"
           >
             <img src="/icons/close.svg" alt="Close icon" className="mr-2 w-5" />
           </button>
         </div>
+
         <h2 id="mobile-menu-heading" className="sr-only">
           Mobile navigation menu
         </h2>
-        <ul>
-          {navLinks.map((link) => (
-            <li key={link.label}>
-              <Link
-                href={link.href}
-                onClick={toggleMenu}
-                className="block py-2 text-bodyLarge text-tertiary1"
-              >
-                {link.label}
-              </Link>
-            </li>
-          ))}
+
+        {/* Menu Links Container */}
+        <div className="flex flex-col space-y-4">
+          <ul>
+            {navLinks.map((link) => (
+              <li key={link.label}>
+                <Link
+                  href={link.href}
+                  onClick={toggleMenu}
+                  className="block py-2 text-bodyLarge text-tertiary1"
+                >
+                  {link.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
           <hr className="my-2 border-t-slate-300" />
-          <div className="mt-12 flex col-6">
-            <LanguageSwitch />
+        </div>
+
+        {/* Language Switch Container */}
+        <div className="mt-12">
+          <LanguageSwitch />
+        </div>
+
+        {/* Follow Us Container */}
+        <div className="pt-12">
+          <h4 className="pb-3 font-semibold footerHeading">Follow Us On</h4>
+          <div className="flex gap-4 m-1" aria-label="Social media icons">
+            <a href="#" className="text-black" aria-label="Instagram">
+              <img
+                src="/icons/instagram-original.svg"
+                className="h-4 filter brightness-0"
+                alt="Instagram"
+              />
+            </a>
+            <a href="#" className="text-black" aria-label="LinkedIn">
+              <img
+                src="/icons/linkedin-alt.svg"
+                className="h-4 filter brightness-0"
+                alt="LinkedIn"
+              />
+            </a>
+            <a href="#" className="text-black" aria-label="Facebook">
+              <img
+                src="/icons/facebook-line.svg"
+                className="h-4 filter brightness-0"
+                alt="Facebook"
+              />
+            </a>
           </div>
-          <div className="pt-12">
-            <h4 className="pb-3 font-semibold footerHeading">Follow Us On</h4>
-            <div
-              className="flex gap-4 m-1 relative right-1"
-              aria-label="Social media icons"
-            >
-              <a href="#" className="text-black" aria-label="Instagram">
-                <img
-                  src="/icons/instagram-original.svg"
-                  className="h-4 filter brightness-0"
-                  alt="Instagram"
-                />
-              </a>
-              <a href="#" className="text-black" aria-label="LinkedIn">
-                <img
-                  src="/icons/linkedin-alt.svg"
-                  className="h-4 filter brightness-0"
-                  alt="LinkedIn"
-                />
-              </a>
-              <a href="#" className="text-black" aria-label="Facebook">
-                <img
-                  src="/icons/facebook-line.svg"
-                  className="h-4 filter brightness-0"
-                  alt="Facebook"
-                />
-              </a>
-            </div>
-          </div>
-        </ul>
+        </div>
       </div>
     </div>
   );
