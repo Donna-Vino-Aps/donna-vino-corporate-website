b08738c956eb1127ed961346753e0aeda42848d9
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-01-16 14:51:23 +0100

    fix: reduce and refactor props + responsivity fixes

--- src/components/ContentGrid/ThematicCardsValues.js
+++ src/components/ContentGrid/ThematicCardsValues.js
@@ -8,48 +8,44 @@ const ThematicCardsValues = () => {
   const { translations } = useLanguage();
   return (
     <div
-      className="flex flex-col justify-center items-center -mt-16 mb-4 lg:mb-8 lg:mx-1"
+      className="flex flex-col justify-center items-center mx-1 -mt-12 mb-4 lg:-mt-8 lg:mb-8 lg:mx-2"
       data-testid="thematicGrid"
     >
       <div
-        className="grid grid-cols-1 sm:grid-cols-2 md:gap-3 lg:grid-cols-1 gap-2"
+        className="flex flex-wrap justify-around w-full gap-x-4 gap-y-2 lg:gap-2"
         data-testid="thematicGrid"
       >
         <ThematicCard
           imageUrl="/images/thematic-cards/thematic-territoriality.png"
-          imgPos="left"
+          cardVariant="variant1"
           smallCardSize="small"
           title={translations["thematic.h1"]}
           descriptionStart={translations["thematic.p1-start"]}
           descriptionEnd={translations["thematic.p1-end"]}
-          backgroundColor="#E8ECE9"
         />
         <ThematicCard
           imageUrl="/images/thematic-cards/thematic-craftsmanship.png"
-          imgPos="right"
+          cardVariant="variant2"
           smallCardSize="small2"
           title={translations["thematic.h2"]}
           descriptionStart={translations["thematic.p2-start"]}
           descriptionEnd={translations["thematic.p2-end"]}
-          backgroundColor="#B7C3BC"
         />
         <ThematicCard
           imageUrl="/images/thematic-cards/thematic-yeast.png"
-          imgPos="left"
+          cardVariant="variant1"
           smallCardSize="big"
           title={translations["thematic.h3"]}
           descriptionStart={translations["thematic.p3-start"]}
           descriptionEnd={translations["thematic.p3-end"]}
-          backgroundColor="#E8ECE9"
         />
         <ThematicCard
           imageUrl="/images/thematic-cards/thematic-minimal.png"
-          imgPos="right"
+          cardVariant="variant2"
           smallCardSize="medium"
           title={translations["thematic.h4"]}
           descriptionStart={translations["thematic.p4-start"]}
           descriptionEnd={translations["thematic.p4-end"]}
-          backgroundColor="#B7C3BC"
         />
       </div>
     </div>
