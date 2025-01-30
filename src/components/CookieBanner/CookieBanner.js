c6ab83ee7244fa9697ed41f62cbf1d34e893503c
lenin del rio <lenindelrionicaragua@gmail.com>
2025-01-30 12:54:15 +0100

    feat: add styles to the CookieBanner.js component.

--- src/components/CookieBanner/CookieBanner.js
+++ src/components/CookieBanner/CookieBanner.js
@@ -1,6 +1,6 @@
 "use client";
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import Button from "../Button/Button";
 
 const CookieBanner = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -20,33 +20,32 @@ const CookieBanner = () => {
   if (!isVisible) return null;
 
   return (
-    <div className="fixed bottom-4 left-4 right-4 md:left-8 md:right-8 p-4 md:p-6 bg-secondary-normal text-white rounded-2xl shadow-lg flex flex-col md:flex-row items-center justify-between gap-4">
+    <div className="fixed bottom-4 left-4 right-4 md:left-8 md:right-8 p-4 md:p-6 bg-white opacity-98 rounded-2xl shadow-lg flex flex-col md:flex-row items-center justify-between gap-4 z-[9999]">
       <p className="text-bodyMedium font-medium">
         We use cookies to improve your experience. Read our
         <a
           href="/privacy-policy"
           className="text-primary-normal hover:text-primary-hover ml-1 underline"
         >
-          {" "}
           Privacy Policy
         </a>
         .
       </p>
       <div className="flex gap-2">
-        <button
+        <Button
+          text="Accept"
+          variant="redSubmit"
           onClick={() => handleConsent("accepted")}
-          className="px-4 py-2 bg-primary-normal hover:bg-primary-hover text-white font-medium rounded-lg"
-          aria-label="Accept cookies"
-        >
-          Accept
-        </button>
-        <button
+          ariaLabel="Accept Cookies"
+          testId="accept-button"
+        />
+        <Button
+          text="Reject"
+          variant="redLine"
           onClick={() => handleConsent("rejected")}
-          className="px-4 py-2 bg-tertiary1-dark hover:bg-tertiary1-hover text-white font-medium rounded-lg"
-          aria-label="Reject cookies"
-        >
-          Reject
-        </button>
+          ariaLabel="Reject Cookies"
+          testId="reject-button"
+        />
       </div>
     </div>
   );
