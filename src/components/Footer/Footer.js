6446d784c965f839d25ba4caaba5b72ff3f55413
YuejiaoShi <shiyuejiaosyj@gmail.com>
2025-02-05 16:00:45 +0100

    feat: add a privacy policy link in footer to its corresponding page

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,24 +7,6 @@ import { useLanguage } from "@/app/context/LanguageContext";
 const Footer = () => {
   const { translations } = useLanguage();
 
-  const links = [
-    {
-      href: "/our-team",
-      label: translations["footer.team"],
-      dataTestId: "our-team",
-    },
-    {
-      href: "/our-values",
-      label: translations["footer.values"],
-      dataTestId: "our-values",
-    },
-    {
-      href: "/contact",
-      label: translations["footer.contact"],
-      dataTestId: "contact",
-    },
-  ];
-
   const socialLinks = [
     {
       href: "https://www.facebook.com/donnavino.dk/",
@@ -45,11 +27,11 @@ const Footer = () => {
 
   return (
     <footer
-      className="relative flex flex-col text-white text-center py-4 md:mt-2 h-96 bg-[#2F2E2E] md:h-[26.625rem] items-center justify-center"
+      className="relative flex flex-col text-white text-center py-4 md:mt-2 h-100 bg-[#2F2E2E] md:h-[26.625rem] items-center justify-center"
       data-testid="footer"
       aria-label="Footer"
     >
-      <div className="flex flex-col relative items-center gap-1 bottom-3 md:mb-32 md:flex-row md:gap-6 lg:gap-9 xl:gap-12">
+      <div className="flex flex-col relative md:items-start items-center gap-1 bottom-3 md:mb-32 md:flex-row md:gap-6 lg:gap-9 xl:gap-12">
         <Link href="/" className="navbar-brand" aria-label="logo">
           <img
             className="h-[5.351rem] w-[7.75rem] rounded relative mt-6 mb-4 md:mt-0 md:mb-0 md:right-4 md:top-6"
@@ -59,21 +41,48 @@ const Footer = () => {
           />
         </Link>
 
-        {links.map(({ href, label, dataTestId }, index) => (
-          <Link
-            key={index}
-            data-testid={dataTestId}
-            href={href}
-            className={`rounded-md px-3 py-2 text-bodyLarge text-semibold order-${index + 2} md:order-${index + 1}`}
-            role="navigation"
-            aria-label={`Link to ${label}`}
-          >
-            {label}
-          </Link>
-        ))}
+        <Link
+          data-testid="our-team"
+          href="/our-team"
+          className={`rounded-md px-3 py-2  text-bodyLarge text-semibold order-2 md:order-1`}
+          role="navigation"
+          aria-label={`Link to ${translations["footer.team"]}`}
+        >
+          {translations["footer.team"]}
+        </Link>
 
-        <div className="flex flex-col order-1 md:order-4 items-center md:relative md:top-5">
-          <h4 className="text-bodyLarge text-semibold mb-1 md:mb-3 md:mt-3">
+        <Link
+          data-testid="our-values"
+          href="/our-values"
+          className={`rounded-md px-3 py-2 text-bodyLarge text-semibold order-3 md:order-2`}
+          role="navigation"
+          aria-label={`Link to ${translations["footer.values"]}`}
+        >
+          {translations["footer.values"]}
+        </Link>
+
+        <div className="flex flex-col order-4 md:order-3">
+          <Link
+            data-testid="contact"
+            href="/contact"
+            className={`rounded-md px-3 py-2 text-bodyLarge text-semibold`}
+            role="navigation"
+            aria-label={`Link to ${translations["footer.contact"]}`}
+          >
+            {translations["footer.contact"]}
+          </Link>
+          <a
+            className="text-bodySmall md:text-bodyMedium text-tertiary2-hover_dark underline md:mt-3 md:mb-1 mt-1 mb-6"
+            href="/privacy-policy"
+            role="navigation"
+            aria-label={`Link to ${translations["footer.privacy-policy"]}`}
+          >
+            {translations["footer.privacy-policy"]}
+          </a>
+        </div>
+
+        <div className="flex flex-col order-1 md:order-5 items-center md:relative">
+          <h4 className="text-bodyLarge text-semibold px-3 py-2">
             {translations["footer.follow"]}
           </h4>
           <div
