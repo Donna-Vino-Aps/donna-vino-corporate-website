8a8793dbb9da16c86530658e02d74b3e3a41611a
lenin del rio <lenindelrionicaragua@gmail.com>
2025-02-17 11:31:22 +0100

    fix: add a fallback for fs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false,
-      };
-    }
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    config.module.rules.push({
+      test: "/\.mjs$/",
+      include: "/node_modules/",
+      type: "javascript/auto",
+    });
     return config;
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
