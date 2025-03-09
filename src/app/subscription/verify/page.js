aee81eae587e5aa6400efb5fb122f9107da25ded
Natalia Lapina <lapinann08@gmail.com>
2025-03-09 16:23:53 +0100

    fix: delete unnecessary tailwind classes

--- src/app/subscription/verify/page.js
+++ src/app/subscription/verify/page.js
@@ -1,12 +1,13 @@
 "use client";
 
+import React from "react";
 import Button from "@/components/Button/Button";
 
 const VerifyEmail = () => {
   return (
-    <section className="lg:my-4 flex flex-col justify-center items-center font-barlow bg-primary-light sm:bg-dots-lg sm:bg-dots-size-lg bg-dots-sm bg-dots-size-sm">
-      <div className="flex flex-col justify-center items-center sm:py-24 py-4 mx-2 max-w-[44rem]">
-        <h1 className="text-displaySmall sm:text-displayMedium font-barlow font-normal text-center leading-tight mb-4 sm:mb-6 md:mb-8">
+    <section className="lg:my-4 flex flex-col justify-center items-center bg-primary-light sm:bg-dots-lg sm:bg-dots-size-lg bg-dots-sm bg-dots-size-sm">
+      <div className="flex flex-col justify-center items-center sm:py-24 py-4 mx-2 max-w-[45rem]">
+        <h1 className="text-displaySmall sm:text-displayMedium text-center mb-4 sm:mb-6 md:mb-8">
           Finalize your subscription
         </h1>
 
