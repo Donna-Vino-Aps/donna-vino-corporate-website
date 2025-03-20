2560eeddae188ae9db5f2cf9ffc3458e59212963
SimonFromSweden <144449218+SimonFromSweden@users.noreply.github.com>
2025-03-20 07:45:28 +0100

    style: small size changes for mobile version of buttons in modal

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -11,13 +11,13 @@ const BASE_BUTTON_CLASSES = `
 const VARIANT_CLASSES = {
   red: "bg-primary-normal hover:bg-primary-hover_normal text-primary-light w-full sm:w-[10.8rem]",
   redModal:
-    "bg-primary-normal hover:bg-primary-hover_normal text-primary-light w-full w-[11rem] h-[2.4rem] text-bodyMedium sm:text-bodyLarge sm:h-[3rem] sm:w-[7.375rem]",
+    "bg-primary-normal hover:bg-primary-hover_normal text-primary-light w-full min-w-[5.6rem] h-[2.4rem] text-bodyMedium sm:text-bodyLarge sm:h-[3rem] sm:w-[7.375rem]",
   redLine:
     "bg-transparent border-2 hover:bg-primary-hover border-primary-active_normal text-primary-active_normal w-full sm:w-[10.8rem]",
   darkGreen:
     "bg-secondary-darker hover:bg-secondary-hover_dark text-white w-full sm:w-[10.8rem]",
   darkGreenModal:
-    "bg-secondary-darker hover:bg-secondary-hover_dark text-white w-full sm:w-[10.8rem] h-[2.4rem] text-bodyMedium sm:text-bodyLarge sm:h-[3rem] sm:h-100",
+    "bg-secondary-darker hover:bg-secondary-hover_dark text-white w-full min-w-[8.8rem] sm:w-[10.8rem] h-[2.4rem] text-bodyMedium sm:text-bodyLarge sm:h-[3rem] sm:h-100",
   grayGreen:
     "bg-secondary-light hover:bg-secondary-hover text-secondary-darker w-full sm:w-[10.8rem]",
   greenSubmit:
