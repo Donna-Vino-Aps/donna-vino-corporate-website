4871cb9de22face65757fd789ae179e09102d5d7
Alejandro Gispert <alejandrobusiness2022@gmail.com>
2025-05-28 12:31:45 +0200

    feat: add umami

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Script from "next/script";
 import "./globals.css";
 import Footer from "../components/Footer/Footer.js";
 import Navbar from "../components/Navbar/Navbar.js";
 import { LanguageProvider } from "./context/LanguageContext";
 import CookieBanner from "../components/CookieBanner/CookieBanner";
+
 export const metadata = {
   title: "Donna Vino",
   description:
@@ -17,12 +19,19 @@ const RootLayout = ({ children }) => {
       <LanguageProvider>
         <body className="flex flex-col min-h-screen w-full font-barlow bg-white text-foreground-normal">
           <Navbar />
-
           <main className="flex-grow" role="main" data-testid="main-content">
             {children}
           </main>
           <CookieBanner />
           <Footer />
+
+          {process.env.NEXT_PUBLIC_UMAMI_ENABLED === "true" && (
+            <Script
+              defer
+              src="https://cloud.umami.is/script.js"
+              data-website-id="26acc54d-5e47-483a-b38c-3c39a489f00c"
+            />
+          )}
         </body>
       </LanguageProvider>
     </html>
